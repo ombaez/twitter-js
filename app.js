2d47b568641bafac5205cd7f7e692b9b04791b55
ombaez <ombaez5689@gmail.com>
2019-01-30 12:50:16 -0300

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require( 'express' );
+const app = express();
+var port = 3000
+
+app.use(Logger)
+
+app.get('/', function (req,res){
+res.send('Root')
+})
+
+app.get('/user', function (req,res){
+res.send('Root/Usuarios')
+})
+
+app.get('/user/:id', Matcher)
+
+
+function Logger (req,res,next)
+{console.log('Logger')
+next()
+}
+
+function Matcher (req,res) {
+    var id = req.params.id
+    res.json(user[id])
+}
+
+app.listen(port,function(){
+console.log('Listening on port ${port}!')
+})
